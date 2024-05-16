8443fc16488123a36ca6332b104e2fc70c929734
AngkurMondal <angkur002@gmail.com>
2024-05-17 00:20:06 +0600

    completed the project

---
+++ Project-6. Todo/script.js
@@ -0,0 +1,25 @@
+const todoList = document.getElementById('todoList');
+const newTodoInput = document.getElementById('newToDoInput');
+const addTodoBtn = document.getElementById('addToDoBtn');
+
+addTodoBtn.addEventListener('click', () => {
+    const newToDoText = newTodoInput.value;
+
+    if (newToDoText) {
+        const newToDoItem = document.createElement('li');
+        newToDoItem.innerText = newToDoText;
+        const deleteTodoBtn = document.createElement('button');
+        deleteTodoBtn.innerText = 'X';
+        deleteTodoBtn.classList.add('delete-todo-btn');
+
+        deleteTodoBtn.addEventListener('click', () => {
+            newToDoItem.remove();
+        });
+
+        newToDoItem.appendChild(deleteTodoBtn);
+        todoList.appendChild(newToDoItem);
+
+        newTodoInput.value = '';
+        newTodoInput.focus();
+    }
+});
